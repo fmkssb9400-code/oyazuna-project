1014423382aca6b1c89803908bc2c4ee332855b3
ryohei nitta <nitta@builwing.com>
2026-05-24 23:23:56 +0900

    update oyazuna

--- resources/views/components/company-tabs-cards.blade.php
+++ resources/views/components/company-tabs-cards.blade.php
@@ -718,9 +718,9 @@
                       <a href="{{ $item['url'] ?? '#' }}"
                          class="flex gap-4 hover:opacity-80 transition">
 
-                          <div class="w-24 h-16 bg-blue-100 rounded flex items-center justify-center flex-shrink-0 overflow-hidden">
+                          <div class="w-28 h-20 bg-blue-100 rounded flex items-center justify-center flex-shrink-0 overflow-hidden">
                               @if(!empty($item['featured_image_url']))
-                                  <img src="{{ $item['featured_image_url'] }}" alt="{{ $item['title'] }}" class="w-16 h-16 object-contain">
+                                  <img src="{{ $item['featured_image_url'] }}" alt="{{ $item['title'] }}" class="w-full h-full object-cover">
                               @else
                                   <svg class="w-8 h-8 text-blue-600" fill="none" stroke="currentColor" viewBox="0 0 24 24">
                                       <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M19 20H5a2 2 0 01-2-2V6a2 2 0 012-2h10a2 2 0 012 2v1m2 13a2 2 0 01-2-2V7m2 13a2 2 0 002-2V9.5a2 2 0 00-2-2h-2m-4-3H9M7 16h6M7 8h6v4H7V8z"/>
